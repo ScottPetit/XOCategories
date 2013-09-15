e2fe17dbf4c6dcdd6dce1c8632be94bf9f62df3e
Scott Petit <scott.petit@bottlerocketapps.com>
2013-09-15 18:36:03 +0300

    Adds UIFont+XOAdditions

---
+++ XOCategories/UIKit/UIFont+XOAdditions.h
@@ -0,0 +1,13 @@
+//
+//  UIFont+XOAdditions.h
+//  XOCategories
+//
+//  Created by Scott Petit on 9/15/13.
+//  Copyright (c) 2013 Scott Petit. All rights reserved.
+//
+
+#define XOFont(name, size) [UIFont fontWithName:name size:size]
+
+@interface UIFont (XOAdditions)
+
+@end
