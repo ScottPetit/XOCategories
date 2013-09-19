239fc85db6abe7e648afb65a1e215171e72dcc15
Scott Petit <scott.petit@bottlerocketapps.com>
2013-09-19 11:07:07 +0300

    Updates to remove warning

--- XOCategories/Foundation/NSString+XOAdditions.m
+++ XOCategories/Foundation/NSString+XOAdditions.m
@@ -15,7 +15,7 @@
 {
     const char *cStr = [self UTF8String];
     unsigned char result[CC_MD5_DIGEST_LENGTH];
-    CC_MD5( cStr, strlen(cStr), result ); 
+    CC_MD5( cStr, (CC_LONG)strlen(cStr), result );
     return [NSString stringWithFormat:
             @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
             result[0], result[1], result[2], result[3],
